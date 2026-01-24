8f1cb9f170c8e6270d968fb74fc94f7b6bb3882f
gooseish <cegooseish@gmail.com>
2026-01-24 09:38:12 -0500

    make it possible to edit fields

--- MolluskEditor/ViewModels/TerrainEditorViewModel.cs
+++ MolluskEditor/ViewModels/TerrainEditorViewModel.cs
@@ -11,9 +11,22 @@ public partial class TerrainEditorViewModel : EditorViewModel
 {
     [ObservableProperty]
     private int _selectedTerrain;
+    partial void OnSelectedTerrainChanged(int oldValue, int newValue)
+    {
+        Avoid = TerrainData[SelectedTerrain].Avoid;
+    }
     
     [ObservableProperty]
     private ObservableCollection<TerrainDataViewModel> _terrainData;
+
+    [ObservableProperty]
+    private int? _avoid;
+    partial void OnAvoidChanged(int? oldValue, int? newValue)
+    {
+        if (newValue == null)
+            return;
+        TerrainData[SelectedTerrain].Avoid = (int)newValue;
+    }
 
     public TerrainEditorViewModel()
     {
@@ -32,6 +45,7 @@ public partial class TerrainEditorViewModel : EditorViewModel
     private void AddTerrainData()
     {
         TerrainData.Add(new TerrainDataViewModel());
+        SelectedTerrain = TerrainData.Count - 1;
     }
     [RelayCommand]
     private void SelectTerrain()
