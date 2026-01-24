7e5d998fec1049b72852a50496c431ddb1663079
gooseish <cegooseish@gmail.com>
2026-01-24 15:28:26 -0500

    minor cleanup

--- MolluskEditor/Services/SaveLoadService.cs
+++ MolluskEditor/Services/SaveLoadService.cs
@@ -11,7 +11,8 @@ namespace MolluskEditor.Services;
 public static class SaveLoadService
 {
     public static string? ContentRoot = @"C:/Users/Home/Documents/Monogame Projects/Mollusk/MolluskEngine/Content/"; // Shouldn't be hardcoded
-    public static string TerrainPath = @"Data/";
+    public static readonly string DATAPATH = @"Data/";
+    public static readonly string TERRAINPATH = "TerrainData.json";
     public static void Save()
     {
         if (ContentRoot == null)
@@ -23,11 +24,11 @@ public static class SaveLoadService
             Converters = {new Array2DConverter()},
         };
         string jsonString = JsonSerializer.Serialize(TerrainDataModel.TerrainData, options);
-        File.WriteAllText(ContentRoot + TerrainPath + "TerrainData.json", jsonString);
+        File.WriteAllText(ContentRoot + DATAPATH + TERRAINPATH, jsonString);
     }
     public static void Open()
     {
-        string jsonString = File.ReadAllText(ContentRoot + TerrainPath + "TerrainData.json");
+        string jsonString = File.ReadAllText(ContentRoot + DATAPATH + TERRAINPATH);
         Dictionary<int, Terrain>? TerrainData = JsonSerializer.Deserialize<Dictionary<int, Terrain>>(jsonString);
         if (TerrainData != null)
             TerrainDataModel.TerrainData = TerrainData;
