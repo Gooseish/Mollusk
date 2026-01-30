a4e4bf6f8a7ef703d7ed8b2a4d10995edbc11591
gooseish <cegooseish@gmail.com>
2026-01-29 19:05:08 -0500

    commandsequence

---
+++ MolluskEditor/Commands/CommandSequence.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MolluskEditor.Commands;
+
+/// <summary>
+/// Object that encapsulates a sequence of consecutive commands
+/// </summary>
+public class CommandSequence : Command
+{
+    private List<Command> _commands = [];
+    /// <summary>
+    /// When Do() is first called, the command queue is
+    /// calcified, preventing further modifications to the
+    /// list of commands.
+    /// </summary>
+    private bool _calcified;
+    public void Add(Command command)
+    {
+        if (_calcified)
+            throw new Exception("Tried to add commands to an already executed command sequence");
+        _commands.Add(command);
+    }
+
+    public override void Do()
+    {
+        _calcified = true;
+        foreach(Command command in _commands)
+        {
+            command.Do();
+        }
+    }
+
+    public override void Undo()
+    {
+        for (int n = _commands.Count; n > 0; --n)
+            _commands[n].Undo();
+    }
+}
