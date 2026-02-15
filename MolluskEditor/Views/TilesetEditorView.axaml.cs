d8bee4d52c4c4c240f57b3c466c0fddf2d371852
gooseish <cegooseish@gmail.com>
2026-02-15 10:32:51 -0500

    only paint terrain with the left mouse button

--- MolluskEditor/Views/TilesetEditorView.axaml.cs
+++ MolluskEditor/Views/TilesetEditorView.axaml.cs
@@ -16,17 +16,33 @@ public partial class TilesetEditorView : UserControl
         PointerPressed += OnPointerPressed;
         PointerMoved += OnPointerMoved;
         PointerReleased += OnPointerReleased;
+        
     }
     private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
+        if (e.GetCurrentPoint(sender as Control).Properties.IsLeftButtonPressed)
+            OnLeftMouseButtonPressed(e);
+        if (e.GetCurrentPoint(sender as Control).Properties.IsRightButtonPressed)
+            OnRightMouseButtonPressed(e);
+    }
+    private void OnLeftMouseButtonPressed(PointerPressedEventArgs e)
+    {
+        // Terrain painter
         _isDrawing = true;
         _startPoint = e.GetPosition(DrawingCanvas);
         if (DataContext is TilesetEditorViewModel tilesetEditorViewModel)
             tilesetEditorViewModel.PaintTilemap(e.GetPosition(DrawingCanvas));
     }
+    private void OnRightMouseButtonPressed(PointerPressedEventArgs e)
+    {
+        // Terrain picker
+    }
     private void OnPointerMoved(object? sender, PointerEventArgs e)
     {
         if (!_isDrawing){ return; }
+        if (!e.GetCurrentPoint(sender as Control).Properties.IsLeftButtonPressed)
+            return;
+        // Terrain painter
         if (DataContext is TilesetEditorViewModel tilesetEditorViewModel)
             tilesetEditorViewModel.PaintTilemap(e.GetPosition(DrawingCanvas));
     }
