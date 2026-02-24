f5f60060dbfed4e0011e44d563146475b40a7ac3
gooseish <cegooseish@gmail.com>
2026-02-23 21:01:23 -0500

    valuetuple json serialization for map data

---
+++ JsonPipeline/ValueTupleConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+public class ValueTupleConverter<T1, T2> : JsonConverter<(T1, T2)>
+    where T1 : new()
+    where T2 : new()
+{
+    public override (T1, T2) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var jArray = JsonSerializer.Deserialize<JsonArray> (ref reader, options);
+        T1 item1 = JsonSerializer.Deserialize<T1>(jArray[0].ToJsonString());
+        T2 item2 = JsonSerializer.Deserialize<T2>(jArray[1].ToJsonString());
+        return (item1, item2);
+    }
+    public override void Write(Utf8JsonWriter writer, (T1, T2) value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+        JsonSerializer.Serialize(writer, value.Item1, options);
+        JsonSerializer.Serialize(writer, value.Item2, options);
+        writer.WriteEndArray();
+    }
+}
--- MolluskEditor/Services/SaveLoadService.cs
+++ MolluskEditor/Services/SaveLoadService.cs
@@ -13,12 +13,16 @@ public class SaveLoadService
     public static readonly string TILESETIMAGES = @"Graphics/Tilesets/";
     private static readonly string TERRAINPATH = "TerrainData.json";
     private static readonly string TILESETPATH = "TilesetData.json";
+    private static readonly string MAPPATH = "MapData.json";
     private DataModel<Terrain> _terrainData;
     private DataModel<Tileset> _tilesetData;
-    public SaveLoadService(DataModel<Terrain> terrainData, DataModel<Tileset> tilesetData)
+    private DataModel<GameMap> _mapData;
+    public SaveLoadService(DataModel<Terrain> terrainData, DataModel<Tileset> tilesetData,
+        DataModel<GameMap> mapData)
     {
         _terrainData = terrainData;
         _tilesetData = tilesetData;
+        _mapData = mapData;
     }
     public void Save()
     {
@@ -28,21 +32,23 @@ public class SaveLoadService
         var options = new JsonSerializerOptions
         {
             WriteIndented = true,
-            Converters = {new Array2DConverter()},
+            Converters = {new Array2DConverter(), new ValueTupleConverter<int, int>()},
         };
         
         _terrainData.Write(CONTENTROOT + DATAPATH + TERRAINPATH, options);
         _tilesetData.Write(CONTENTROOT + DATAPATH + TILESETPATH, options);
+        _mapData.Write(CONTENTROOT + DATAPATH + MAPPATH, options);
     }
     public void Open()
     {
         var options = new JsonSerializerOptions
         {
-            Converters = {new Array2DConverter()},
+            Converters = {new Array2DConverter(), new ValueTupleConverter<int, int>()},
         };
         
         _terrainData.Read(CONTENTROOT + DATAPATH + TERRAINPATH, options);
         _tilesetData.Read(CONTENTROOT + DATAPATH + TILESETPATH, options);
+        _mapData.Read(CONTENTROOT + DATAPATH + MAPPATH, options);
         
         OnProjectLoaded();
     }
